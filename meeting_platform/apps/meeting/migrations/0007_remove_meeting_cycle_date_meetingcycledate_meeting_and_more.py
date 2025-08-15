d1f4d9e91b6e546fc1a807c699f1cc7fcfefb930
TomNewChao <tom_toworld@163.com>
2025-08-15 15:54:38 +0800

    fix bug about add the 007 migrations

--- meeting_platform/apps/meeting/migrations/0007_remove_meeting_cycle_date_meetingcycledate_meeting_and_more.py
+++ meeting_platform/apps/meeting/migrations/0007_remove_meeting_cycle_date_meetingcycledate_meeting_and_more.py
@@ -11,10 +11,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RemoveField(
-            model_name='meeting',
-            name='cycle_date',
-        ),
         migrations.AddField(
             model_name='meetingcycledate',
             name='meeting',
