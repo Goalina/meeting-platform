3fa551080687ffe78a56387beefd61cb8b00b804
TomNewChao <tom_toworld@163.com>
2025-08-26 15:45:08 +0800

    adapter the sub info

--- meeting_platform/apps/meeting/application/obs_records_app.py
+++ meeting_platform/apps/meeting/application/obs_records_app.py
@@ -20,7 +20,12 @@ class OBSRecordsApp:
         meeting_obj = self._meeting_dao.get_by_mid(meeting_info["mid"])
         if not meeting_obj:
             raise MyValidationError(RetCode.STATUS_MEETING_NOT_EXIST)
-        return self._meeting_obs_records_dao.update_by_mid(meeting_info.pop("mid"),
-                                                           meeting_info.get("sub_id"),
+        mid, sub_id = None, None
+        if "mid" in meeting_info.keys():
+            mid = meeting_info.pop("mid")
+        if "sub_id" in meeting_info.keys():
+            sub_id = meeting_info.pop("sub_id")
+        return self._meeting_obs_records_dao.update_by_mid(mid,
+                                                           sub_id,
                                                            UploadStatus.FINISH.value,
                                                            **meeting_info)
